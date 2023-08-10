5b49e9d9bedef32334dc42875327758033c687d5
cpolic <curtis.polic@gmail.com>
2023-08-10 14:51:21 +1000

    configuring git with netbeans

---
+++ sweeper/src/main/java/curtis/sweeper/Sweeper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package curtis.sweeper;
+
+/**
+ *
+ * @author curti
+ */
+public class Sweeper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
